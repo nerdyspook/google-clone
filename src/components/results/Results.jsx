e1637a49af98a0e9f6dca2646034ccc6d146fb85
Susanto <nerdyspook007@gmail.com>
2021-11-22 21:12:17 +0530

    video section added

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -6,19 +6,15 @@ import { useResultContext } from "../../contexts/ResultContextProvider";
 import { Loading } from "..";
 
 const Results = () => {
-    const {
-        results: { results, image_results },
-        isLoading,
-        getResults,
-        searchTerm,
-    } = useResultContext();
+    const { results, isLoading, getResults, searchTerm } = useResultContext();
     const location = useLocation();
 
     useEffect(() => {
         if (searchTerm) {
-            if (location.pathname === "/videos") {
+            if (location.pathname == "/videos") {
                 getResults(`/search/q=${searchTerm} videos`);
             } else {
+                // console.log(searchTerm);
                 getResults(`${location.pathname}/q={searchTerm}&num=40`);
             }
         }
@@ -50,31 +46,25 @@ const Results = () => {
         case "/images":
             return (
                 <div className="flex flex-wrap">
-                    {image_results?.map(
-                        ({ image, link: { href, title } }, index) => (
-                            <a
-                                href={href}
-                                key={index}
-                                target="_blank"
-                                rel="noreferrer"
-                                className="sm:p-3 p-5"
-                            >
-                                <img
-                                    src={image?.src}
-                                    alt={title}
-                                    loading="lazy"
-                                />
-                                <p className="w-36 break-words text-sm mt-2"></p>
-                            </a>
-                        )
-                    )}
+                    {results?.map(({ image, link: { href, title } }, index) => (
+                        <a
+                            href={href}
+                            key={index}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="sm:p-3 p-5"
+                        >
+                            <img src={image?.src} alt={title} loading="lazy" />
+                            <p className="w-36 break-words text-sm mt-2"></p>
+                        </a>
+                    ))}
                 </div>
             );
 
         case "/news":
             return (
                 <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
-                    {/* {news?.map(({ links, id, source, title }) => (
+                    {results?.map(({ links, id, source, title }) => (
                         <div key={id} className="md:w-2/5 w-full">
                             <a
                                 className="hover:underline"
@@ -85,18 +75,34 @@ const Results = () => {
                                 <p className="text-lg dark:text-blue-300 text-blue-700">
                                     {title}
                                 </p>
-                                <div className="flex gap-4">
-                                    <a
-                                        href={source?.href}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        {source?.href}
-                                    </a>
-                                </div>
                             </a>
+                            <div className="flex gap-4">
+                                <a
+                                    href={source?.href}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    {source?.href}
+                                </a>
+                            </div>
                         </div>
-                    ))} */}
+                    ))}
+                </div>
+            );
+
+        case "/videos":
+            return (
+                <div className="flex flex-wrap">
+                    {results.map((video, index) => (
+                        <div key={index} className="p-2">
+                            <ReactPlayer
+                                url={video.additional_links?.[0].href}
+                                controls
+                                width="355px"
+                                height="200px"
+                            />
+                        </div>
+                    ))}
                 </div>
             );
 
