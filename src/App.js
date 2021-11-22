a411ce4f9cf0d4d0dbc047cae85b29a1995d825e
Susanto <nerdyspook007@gmail.com>
2021-11-22 14:52:40 +0530

    design and functionality added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Navbar, Footer, Routes } from "./components";
 
 const App = () => {
+    const [darkTheme, setDarkTheme] = useState(false);
+
     return (
-        <div>
-            <h1>App</h1>
+        <div className={darkTheme ? "dark" : ""}>
+            <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+                <Routes />
+                <Footer />
+            </div>
         </div>
     );
 };
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Search } from "./search/Search";
+export { default as Routes } from "./routes/Routes";
+export { default as Results } from "./results/Results";
+export { default as Loading } from "./loading/Loading";
+export { default as Footer } from "./footer/Footer";
